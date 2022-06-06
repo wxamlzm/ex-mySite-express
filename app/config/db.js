d61369ff03aad2dce634d48a866e8e869b5dde4b
wxamlzm <zd5024@163.com>
2022-06-06 10:46:20 +0800

    UPDATE：更新语法

--- app/config/db.js
+++ app/config/db.js
@@ -1,14 +1,11 @@
-// 数据库配置文件
+// 定义数据库
+const mongoose = require('mongoose')
+const mongdbUrl = 'mongodb://localhost:27017/login'
 
-// 引入mongose
-const mongoose = require('mongoose');
-// 定义URL，存放数据库地址
-const mongoUrl = 'mongodb://localhost:27017/login';
-
-// 暴露连接
+// 抛出连接
 module.exports = app => {
-    // 连接数据库
-    mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-        console.log('mongodb connect');
-    });
-};
+  // 连接数据库
+  mongoose.connect(mongdbUrl, () => {
+    console.log('mongodb connect')
+  })
+}
