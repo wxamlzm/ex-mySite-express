93bc4263e7f2a3b91a799f3fdbd02cae2f9dd7c3
wxamlzm <zd5024@163.com>
2022-06-26 21:40:07 +0800

    UPDATE：更新token

--- app/router/user.js
+++ app/router/user.js
@@ -42,7 +42,33 @@ router.post('/login', async (req, res) => {
         return res.status(422).send('密码错误');
     }
 
-    res.send(user);
+    // res.send(user);
+    const token = user._id + '.' + user.username;
+    res.send(token);
+});
+// 验证
+router.get('/verify', async (req, res) => {
+    // req.headers.authorization
+    // 获取token
+    const token = req.headers.authorization.split(' ')[1];
+    const id = token.split('.')[0];
+    const username = token.split('.')[1];
+    // 查询是否存在用户
+    const user = User.findById(id);
+    if (!user) {
+        return res.status(422).send('用户错误');
+    }
+    // 查看usernam
+    if (username !== user.username) {
+        res.status(422).send('用户错误');
+    } else {
+        // 用户存在，查看权限
+        if (user.isAdmin === '0') {
+            res.status(409).send('没有权限');
+        } else if (user.isAdmin === '1') {
+            res.send('Admin');
+        }
+    }
 });
 
 module.exports = router;
