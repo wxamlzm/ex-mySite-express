dad4e345851cd70a8b4717c198715b694ecf62bc
wxamlzm <zd5024@163.com>
2022-07-11 22:28:23 +0800

    UPDATE：以bcrypt替换原加密函数

--- app/model/User.js
+++ app/model/User.js
@@ -1,10 +1,17 @@
 // 测试模型，用于记录注册用户
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
     username: { type: String, require: true },
-    password: { type: String, require: true },
+    password: {
+        type: String,
+        require: true,
+        set(val) {
+            return bcrypt.hashSync(val, 10);
+        },
+    },
     nickname: { type: String, require: true },
     isAdmin: { type: String, default: '0' },
 });
--- app/router/user.js
+++ app/router/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 // 引入数据库表
 const User = require('../model/User');
 const router = express.Router();
@@ -6,8 +7,33 @@ const router = express.Router();
 // 密码加盐
 const salt = '';
 
+// 验证身份-中间件
+const isAdmin = async (req, res, next) => {
+    // 获取token
+    const token = req.headers.authorization.split(' ')[1];
+    const id = token.split('.')[0];
+    const username = token.split('.')[1];
+    // 查询是否存在用户
+    const user = User.findById(id);
+    if (!user) {
+        return res.status(422).send('用户错误');
+    }
+    // 查看usernam
+    if (username !== user.username) {
+        res.status(422).send('用户错误');
+    } else {
+        // 用户存在，查看权限
+        if (user.isAdmin === '0') {
+            res.status(409).send('没有权限');
+        } else if (user.isAdmin === '1') {
+            // res.send('Admin');
+            next();
+        }
+    }
+};
+
 // 查询接口
-router.get('/', async (req, res) => {
+router.get('/', isAdmin, async (req, res) => {
     const list = await User.find();
     res.send(list);
 });
@@ -18,7 +44,7 @@ router.post('/register', async (req, res) => {
     if (user) {
         return res.status(409).send('该用户已存在');
     }
-    req.body.password = setEncode(req.body.password, salt);
+    // req.body.password = setEncode(req.body.password, salt);
 
     const newUser = await new User(req.body).save();
     res.send(newUser);
@@ -37,38 +63,19 @@ router.post('/login', async (req, res) => {
     //     res.send('token');
     // }
     // 解密
-    let isPassword = constrast(req.body.password, user.password, salt);
+    // let isPassword = constrast(req.body.password, user.password, salt);
+    let isPassword = bcrypt.compareSync(req.body.password, user.password);
     if (!isPassword) {
         return res.status(422).send('密码错误');
     }
 
     // res.send(user);
-    const token = user._id + '.' + user.username;
+    const token = user._id + '.' + user.username
     res.send(token);
 });
 // 验证
 router.get('/verify', async (req, res) => {
     // req.headers.authorization
-    // 获取token
-    const token = req.headers.authorization.split(' ')[1];
-    const id = token.split('.')[0];
-    const username = token.split('.')[1];
-    // 查询是否存在用户
-    const user = User.findById(id);
-    if (!user) {
-        return res.status(422).send('用户错误');
-    }
-    // 查看usernam
-    if (username !== user.username) {
-        res.status(422).send('用户错误');
-    } else {
-        // 用户存在，查看权限
-        if (user.isAdmin === '0') {
-            res.status(409).send('没有权限');
-        } else if (user.isAdmin === '1') {
-            res.send('Admin');
-        }
-    }
 });
 
 module.exports = router;
