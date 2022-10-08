93b82edf047cfd0bbb53e73477ae591f2502cbc9
wxamlzm <zd5024@163.com>
2022-10-08 20:20:54 +0800

    UPDATE:添加验证

--- app/router/user.js
+++ app/router/user.js
@@ -2,14 +2,20 @@
  * @Author: zd
  * @Date: 2022-10-08 18:48:30
  * @LastEditors: zd
- * @LastEditTime: 2022-10-08 19:49:55
+ * @LastEditTime: 2022-10-08 20:14:25
  * @Description: 用户模块路由
  */
 const express = require('express')
+// 密码组件
 const bcrypt = require('bcryptjs')
+// token组件
+const jwt = require('jsonwebtoken')
 // 引入数据库表
 const User = require('../model/User')
 const router = express.Router()
+
+// 加密规则
+const secret = 'register-rule'
 
 // 查询接口
 router.get('/', async (req, res) => {
@@ -33,8 +39,34 @@ router.post('/login', async (req, res) => {
   let isPassword = bcrypt.compareSync(req.body.password, user.password)
   if (!isPassword) return res.status(422).send('密码错误')
 
-  const token = user._id + '.' + user.username
+  // const token = user._id + '.' + user.username
+  const { _id, username } = user
+  const token = jwt.sign({ _id, username }, secret, { expiresIn: '24h' })
   res.send(token)
+})
+// 验证路由
+router.get('/verify', async (req, res) => {
+  console.log(req.headers.authorization)
+  // 1. 获取token
+  const token = req.headers.authorization.split(' ')[1]
+  const id = token.split('.')[0]
+  const username = token.split('.')[1]
+  // 2.查询用户是否存在
+  const user = await User.findById(id)
+  if (!user) {
+    return res.status(422).send('用户错误')
+  }
+  // 3.查看username
+  if (username !== user.username) {
+    res.status(422).send('用户错误')
+  } else {
+    // 用户存在，查看权限
+    if (user.isAdmin === '0') {
+      res.status(409).send('没有权限')
+    } else if (user.isAdmin === '1') {
+      res.send('Admin')
+    }
+  }
 })
 
 module.exports = router
