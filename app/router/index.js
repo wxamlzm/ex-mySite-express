1728dfb6ec4fe3b577d9c169d0e42dec8a884ece
wxamlzm <zd5024@163.com>
2022-10-08 18:57:42 +0800

    UPDATE：修复

--- app/router/index.js
+++ app/router/index.js
@@ -2,11 +2,10 @@
  * @Author: zd
  * @Date: 2022-10-08 18:48:30
  * @LastEditors: zd
- * @LastEditTime: 2022-10-08 18:54:45
- * @Description: 
+ * @LastEditTime: 2022-10-08 18:57:23
+ * @Description:
  */
 const userRouter = require('./user')
-const motiveRouter = require('./motive')
 
 module.exports = app => {
   app.use('/user', userRouter)
