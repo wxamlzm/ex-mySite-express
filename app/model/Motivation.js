a9c7dfaa45ca7206a7ad38513279801966f234ef
wxamlzm <zd5024@163.com>
2022-10-08 18:55:31 +0800

    UPDATE:代码格式化

--- app/model/Motivation.js
+++ app/model/Motivation.js
@@ -1,12 +1,12 @@
 // 测试模型，用于记录
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
 
 const MotivationSchema = new Schema({
-    test: { type: String },
-    isAdmin: { type: String, default: '0' },
-});
+  test: { type: String },
+  isAdmin: { type: String, default: '0' }
+})
 
-const Motivation = model('Motivation', MotivationSchema);
+const Motivation = model('Motivation', MotivationSchema)
 
-model.exports = Motivation;
+model.exports = Motivation
--- app/model/User.js
+++ app/model/User.js
@@ -1,21 +1,27 @@
-// 测试模型，用于记录注册用户
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const { Schema, model } = mongoose;
+/*
+ * @Author: zd
+ * @Date: 2022-10-08 18:48:30
+ * @LastEditors: zd
+ * @LastEditTime: 2022-10-08 18:54:54
+ * @Description: 测试模型，用于记录注册用户
+ */
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const { Schema, model } = mongoose
 
 const UserSchema = new Schema({
-    username: { type: String, require: true },
-    password: {
-        type: String,
-        require: true,
-        set(val) {
-            return bcrypt.hashSync(val, 10);
-        },
-    },
-    nickname: { type: String, require: true },
-    isAdmin: { type: String, default: '0' },
-});
+  username: { type: String, require: true },
+  password: {
+    type: String,
+    require: true,
+    set (val) {
+      return bcrypt.hashSync(val, 10)
+    }
+  },
+  nickname: { type: String, require: true },
+  isAdmin: { type: String, default: '0' }
+})
 
-const User = model('User', UserSchema);
+const User = model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
